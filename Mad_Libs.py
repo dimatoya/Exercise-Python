b4787455ef2605cad66ea42469e95d3622139fc4
Diego Mauricio Torres Yanquen <88349489+dimatoya@users.noreply.github.com>
2022-05-20 16:52:57 -0300

    Firsts Jobs

---
+++ Mad_Libs.py
@@ -0,0 +1,17 @@
+#EXERCISE MAD LIBS
+
+#Poem:
+'''Roses are red
+   Violets are blue
+   I love you...'''
+
+print("YOUR SELECTION:\n")
+color = input("\tEnter your color: ")
+plural_noun = input ("\tEnter your plural noun: ")
+celebrity= input("\tEnter your celebrity: ")
+
+print("\nTEST POEM\n")
+print ("\tRoses are " + color + ",")
+print ("\t" + plural_noun + " are blue,")
+print ("\tI love " + celebrity + ".")
+print("\tThank you :)")
